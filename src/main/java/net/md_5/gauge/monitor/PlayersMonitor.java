ffcf3dd95496c09e0d61cb5d4a0d5088612161ba
md_5 <md_5@live.com.au>
2013-02-16 12:13:09 +1100

    Add player join / parts +  online count.

--- src/main/java/net/md_5/gauge/monitor/PlayersMonitor.java
+++ src/main/java/net/md_5/gauge/monitor/PlayersMonitor.java
@@ -1,14 +1,34 @@
 package net.md_5.gauge.monitor;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class PlayersMonitor implements Runnable
 {
 
+    private final Set<String> seen = new HashSet<>();
+
     @Override
     public void run()
     {
         Player[] players = Bukkit.getServer().getOnlinePlayers();
+        Set<String> joined = new HashSet<>();
+        Set<String> left = new HashSet<>( seen );
+
+        for ( Player player : players )
+        {
+            String name = player.getName();
+            if ( !seen.contains( name ) )
+            {
+                joined.add( name );
+            }
+            left.remove( name );
+        }
+
+        int joinedCount = joined.size();
+        int leftCount = left.size();
+        int onlineCount = players.length;
     }
 }
